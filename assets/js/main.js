9ade9ba0aa7f4c533258d40974fcff29719a24e4
Ashish Makwana <85098548+ashishmakwana96am@users.noreply.github.com>
2024-09-24 12:15:20 +0530

    code uploaded

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    let video = document.getElementById("vid");
+    let mediaDevices = navigator.mediaDevices;
+
+    mediaDevices.getUserMedia({
+        video: { facingMode: { exact: "environment" } }, // Use back camera
+        // video: { facingMode: { ideal: "environment" } }, // Use back camera
+        audio: true,
+    })
+    .then((stream) => {
+        // Changing the source of video to current stream.
+        video.srcObject = stream;
+        video.addEventListener("loadedmetadata", () => {
+            video.play();
+        });
+    })
+    .catch(alert);
+});
